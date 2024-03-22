5323d33a7413c7d0231517e1fd35c032a5e37a52
teenage1ove <kirillstars7@gmail.com>
2024-03-22 21:55:10 +0300

    Add data cards

---
+++ scripts/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        id: 1, title: 'Weekend Offender', price: 75, url: 'https://img.brandshop.ru/cache/products/p/ptss2417-blk-0_1104x1104.jpg'
+    },
+    {
+        id: 2, title: 'Jordan', price: 200, url: 'https://img.brandshop.ru/cache/products/d/dx4981-100-0_1104x1104.jpg'
+    },
+    {
+        id: 3, title: 'Jordan', price: 200, url: 'https://img.brandshop.ru/cache/products/d/dx4981-006-0_1104x1104.jpg'
+    },
+    {
+        id: 4, title: 'New Balance', price: 119, url: 'https://img.brandshop.ru/cache/products/m/ml574evb-0_1104x1104.jpg'
+    },
+    {
+        id: 5, title: 'Asics', price: 120, url: 'https://img.brandshop.ru/cache/products/1/1201a842-001-0_1104x1104.jpg'
+    },
+    {
+        id: 6, title: 'Puma', price: 75, url: 'https://img.brandshop.ru/cache/products/3/307855-02-0_1104x1104.jpg'
+    },
+    {
+        id: 7, title: 'Puma', price: 50, url: 'https://img.brandshop.ru/cache/products/3/390087-01-0_1104x1104.jpg'
+    },
+    {
+        id: 8, title: 'C.P. Company', price: 1000, url: 'https://img.brandshop.ru/cache/products/1/16cmkn047a005367m-103-0_1104x1104.jpg'
+    },
+    {
+        id: 9, title: 'C.P. Company', price: 300, url: 'https://img.brandshop.ru/cache/products/1/16cmsh301a005415g-103-0_1104x1104.jpg'
+    },
+    {
+        id: 10, title: 'Nike', price: 250, url: 'https://img.brandshop.ru/cache/products/d/dd1391-104-00_1104x1104.jpg'
+    },
+    {
+        id: 11, title: 'Carhartt WIP', price: 120, url: 'https://img.brandshop.ru/cache/products/i/i031468-667-xx-0_1104x1104.jpg'
+    },
+    {
+        id: 12, title: 'Peaceful Hooligan', price: 180, url: 'https://img.brandshop.ru/cache/products/m/muzhskaya-tolstovka-peaceful-hooligan-outline-dove-hoodie-black-00_1104x1104.jpg'
+    },
+]
