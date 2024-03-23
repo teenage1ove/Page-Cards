0416a861579920dec1f93d20355b16250d2eb298
teenage1ove <kirillstars7@gmail.com>
2024-03-23 18:55:55 +0300

    Add show more btn

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 import variables from './variables.js'
 import {data} from './data.js'
-const {shopContent, navBasket} = variables
+const {shopContent, navBasket, moreBtn} = variables
 
 const toHTML = card => `
-<div class = "shop-card"> 
+<div class = "shop-card ${card.show}"> 
     <img src="${card.url}" alt="${card.title}" class="shop-card__img">
     <h2 class="shop-card__title">${card.title}</h2>
     <span class="shop-card__price">${card.price}$</span>
@@ -12,8 +12,32 @@ const toHTML = card => `
 
 const renderCards = () => {
     const html = data.map(i => toHTML(i)).join('')
-    console.log(html)
     shopContent.innerHTML = html
 }
 
-renderCards()
+let cards = 8
+
+const showCards = (show) => {
+    if (window.innerWidth <= 990) cards = 9
+    if (window.innerWidth <= 740) cards = 8
+    for (let i = cards; i < data.length; i++) {
+        data[i].show = show
+    }
+    renderCards()
+}
+
+const moreCards = () => {
+    if (data.length > cards) {
+        moreBtn.classList.add('active')
+        showCards('none')
+    }
+
+    moreBtn.addEventListener('click', () => {
+        showCards('active')
+        moreBtn.classList.remove('active')
+    })
+
+}
+moreCards()
+
+
--- scripts/variables.js
+++ scripts/variables.js
@@ -1,4 +1,5 @@
 export default {
     navBasket: document.querySelector('.nav__basket'),
     shopContent: document.querySelector('.shop-content'),
+    moreBtn: document.querySelector('.shop__more'),
 }
