ac459bd86f64a08bd59b554433d7c34900495915
teenage1ove <kirillstars7@gmail.com>
2024-03-28 09:05:25 +0300

    Add variables

--- scripts/variables.js
+++ scripts/variables.js
@@ -1,5 +1,14 @@
 export default {
-    navBasket: document.querySelector('.nav__basket'),
+    cart: document.querySelector('.cart'),
+    cartBtn: document.querySelector('.nav__basket'),
+    closeCartBtn: document.querySelector('.cart__close'),
     shopContent: document.querySelector('.shop-content'),
     moreBtn: document.querySelector('.shop__more'),
+    removeCartBtns: document.getElementsByClassName('cart__remove'),
+    quantityInputs: document.getElementsByClassName('cart__quantity'),
+    totalPrice: document.querySelector('.total__price'),
+    cartContent: document.getElementsByClassName('cart__content')[0],
+    cartBoxes: document.getElementsByClassName('cart__box'),
+    addCarts: document.getElementsByClassName('shop-card__basket'),
+    buyBtn: document.getElementsByClassName('cart__buy')[0],
 }
